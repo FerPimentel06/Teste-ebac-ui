f669eda5953bd039bb4272b4609f75a8604d5889
Fernanda Pimentel <ferampli06@gmail.com>
2022-01-28 19:22:44 -0300

    Alterações no teste de automatizado

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -2,8 +2,14 @@
 
 context('Funcionalidade Login', () =>{
 
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    afterEach (() => {
+        cy.screenshot()
+    });
     it('Deve fazer login com sucesso' , () => {
-cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
 cy.get('#username').type('aluno_ebac@teste.com')
 cy.get('#password').type('teste@teste.com')
 cy.get('.woocommerce-form > .button').click()
@@ -12,7 +18,6 @@ cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'OlÃ
     })
 
     it('deve exibir uma mensagem de erro ao inserir usuario invalido' , () =>{
-cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
 cy.get('#username').type('alunno_ebac@teste.com')
 cy.get('#password').type('teste@teste.com')
 cy.get('.woocommerce-form > .button').click()
@@ -21,7 +26,6 @@ cy.get('.woocommerce-error').should('contain' , 'desconhecido')
     })
 
     it('deve exibir uma mensagem de erro ao inserir senha invalida' , () =>{
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('testeteste.com')
         cy.get('.woocommerce-form > .button').click()
