9fbdffd186fedee6bdd98abbb356910c306144ea
Fernanda Pimentel <ferampli06@gmail.com>
2022-02-19 21:58:56 -0300

    Usando massa de dados

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,9 +1,10 @@
 /// <reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () =>{
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
     
     afterEach (() => {
@@ -16,6 +17,12 @@ cy.get('.woocommerce-form > .button').click()
 
 cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá')
     })
+
+    it.only('Deve fazer login com sucesso 2', () => {
+        cy.get('#username').type(perfil.usario)
+cy.get('#password').type(perfil.senha)
+cy.get('.woocommerce-form > .button').click()
+    });
 
     it('deve exibir uma mensagem de erro ao inserir usuario invalido' , () =>{
 cy.get('#username').type('alunno_ebac@teste.com')
--- cypress/integration/pre-cadastro.spec.js
+++ cypress/integration/pre-cadastro.spec.js
@@ -4,10 +4,11 @@ var faker = require('faker');
 describe('Funcionalidade Pré Cadastro' , () =>{
 
     before(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.visit('minha-conta/')
     });
 
     it('Deve completar o pré cadastro com sucesso' , () => {
+        
         let nomeFaker = faker.name.firstName()
         let sobrenomeFaker = faker.name.lastName()
         let emailFaker = faker.internet.email(nomeFaker)
